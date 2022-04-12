cab8211a82bb50f1012f589b2622ff731df167a3
aluvium <aluvium@protonmail.com>
2022-04-12 15:54:56 +0200

    changes

--- tcp_cli.py
+++ tcp_cli.py
@@ -4,11 +4,9 @@ T_PORT=80
 #define client and connect to host
 client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((T_HOST,T_PORT))
-
 #header data --> hes to be in bytes
 info=bytes("GET / HTTP/1.1\r\nHost: example.com\r\n\r\n", "utf-8")
 client.send(info)
-
 # recive data
 response=client.recv(4096)
 print(response)
