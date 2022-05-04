25834779b8701c220a068abf1b80795a6df2a547
aluvium <aluvium@protonmail.com>
2022-05-04 19:58:01 +0200

    -,-

--- url_fetch.py
+++ url_fetch.py
@@ -1,11 +1,13 @@
 import urllib.request
-with urllib.request.urlopen("http://google.com") as response:
+from pprint import pprint
+with urllib.request.urlopen("http://mercury.picoctf.net:47967") as response:
     html=response.read()
+    ## geturl - when directed and header info
     info=response.info()
     url=response.geturl()
-    print(html)
+    pprint(html)
     print("\nThis is info\n%s" % info)
     print("\nThis is real url\n%s" % url)
-#geturl()
-#info()
-
+#    meto=dir(urllib.request.HTTPCookieProcessor.response("https://exmaple.com"))
+#    for each in meto:
+#       print(f"\n{each}")
