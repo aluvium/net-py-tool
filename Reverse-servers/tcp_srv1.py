1eab1adc297a394446fa62bc1a45b7cc4a51d1d2
aluvium <aluvium@protonmail.com>
2022-09-18 08:49:06 +0200

    update Sun Sep 18 08:49:06 CEST 2022

--- Reverse-servers/tcp_srv1.py
+++ Reverse-servers/tcp_srv1.py
@@ -1,12 +1,6 @@
 import socketserver
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
-    """
-    The request handler class for our server.
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
